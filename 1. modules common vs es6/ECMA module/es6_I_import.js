66fd52f8d6bd98a6c041cdf19e7761978a7fbb38
Mahnoor <mahnoorali57se@gmail.com>
2022-11-25 16:41:13 +0500

    common js modules vs ECMA modules

---
+++ 1. modules common vs es6/ECMA module/es6_I_import.js
@@ -0,0 +1,7 @@
+import hello_Or_bye from './es6_exportMe.mjs';
+import * as all from './es6_exportMe.mjs';
+
+console.log(hello_Or_bye());
+console.log(all);
+console.log(all.helloG());
+//console.log(Bye());
---
+++ 1. modules common vs es6/ECMA module/es6_exportMe.mjs
@@ -0,0 +1,8 @@
+export function helloG() {
+    console.log('hello from HelloG');
+    return 5;
+}
+
+export default function Bye() {
+    console.log('Bye G');
+}
